aa38db321ddeaa9e652dc162473467d0cc048f88
Arun Rana <m.xpert786@gmail.com>
2025-12-17 10:05:09 +0530

    changes fixed

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  trailingSlash: true,
-  basePath: '/surepoint-frontend',
-  assetPrefix: '/surepoint-frontend',
+
   images: {
-    unoptimized: true,
     formats: ['image/webp'],
   },
 };
