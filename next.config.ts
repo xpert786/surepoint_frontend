b0e5e3aec76c9f66c2413fe3e61dab248305f722
naman <naman@itinfonity.com>
2025-12-17 14:11:32 +0530

    17-12

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/surepoint-frontend",
+  assetPrefix: "/surepoint-frontend",
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     formats: ["image/webp"],
   },
 };
